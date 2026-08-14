86ca92532ab6ff17343b565cd53dc6c3ebf6f431
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/profile route

---
+++ src/app/(donor)/donor/profile/page.tsx
@@ -0,0 +1,7 @@
+import { DonorProfileView } from "@/features/donor-account/donor-profile";
+
+export const metadata = { title: "Donor Profile | ShareBite BD" };
+
+export default function DonorProfilePage() {
+  return <DonorProfileView />;
+}
