770ba53b41595d24945095b4f6d95c7230079348
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/profile/edit route

---
+++ src/app/(donor)/donor/profile/edit/page.tsx
@@ -0,0 +1,7 @@
+import { ProfileEditor } from "@/features/donor-account/profile-editor";
+
+export const metadata = { title: "Edit Donor Profile | ShareBite BD" };
+
+export default function EditDonorProfilePage() {
+  return <ProfileEditor />;
+}
