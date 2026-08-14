cfaddf3458686d49b0e8dccbdc3bb09ed9a9757a
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/notifications route

---
+++ src/app/(donor)/donor/notifications/page.tsx
@@ -0,0 +1,7 @@
+import { NotificationCenter } from "@/features/donor-account/notification-center";
+
+export const metadata = { title: "Notifications | ShareBite BD" };
+
+export default function DonorNotificationsPage() {
+  return <NotificationCenter />;
+}
