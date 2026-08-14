ce7c070aaef03521e73d49ce58a957326f49dc26
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/addresses route

---
+++ src/app/(donor)/donor/addresses/page.tsx
@@ -0,0 +1,7 @@
+import { AddressManager } from "@/features/donor-account/address-manager";
+
+export const metadata = { title: "Saved Addresses | ShareBite BD" };
+
+export default function DonorAddressesPage() {
+  return <AddressManager />;
+}
