091a1ef6f309ed4868a7b84d9274789f824a3988
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/ngos route

---
+++ src/app/(donor)/donor/ngos/page.tsx
@@ -0,0 +1,7 @@
+import { NGODirectory } from "@/features/ngos/ngo-directory";
+
+export const metadata = { title: "Verified NGOs" };
+
+export default function DonorNGODirectoryPage() {
+  return <NGODirectory />;
+}
