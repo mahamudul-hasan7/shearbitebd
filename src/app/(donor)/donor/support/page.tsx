e25e1981fb7037da568b0915fd2c37902dc85bc3
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/support route

---
+++ src/app/(donor)/donor/support/page.tsx
@@ -0,0 +1,7 @@
+import { DonorSupport } from "@/features/donor-account/donor-support";
+
+export const metadata = { title: "Donor Support | ShareBite BD" };
+
+export default function DonorSupportPage() {
+  return <DonorSupport />;
+}
