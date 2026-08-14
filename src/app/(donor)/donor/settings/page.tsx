8caf5a791af3df591814169670123a057bb80a24
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-14 22:02:01 +0600

    feat: add /donor/settings route

---
+++ src/app/(donor)/donor/settings/page.tsx
@@ -0,0 +1,7 @@
+import { DonorSettings } from "@/features/donor-account/donor-settings";
+
+export const metadata = { title: "Donor Settings | ShareBite BD" };
+
+export default function DonorSettingsPage() {
+  return <DonorSettings />;
+}
