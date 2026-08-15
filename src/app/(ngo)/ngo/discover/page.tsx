c856daa7e429b2855aba408f7d3f0dcc5502d656
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-15 23:39:26 +0600

    feat: add /ngo/discover route

---
+++ src/app/(ngo)/ngo/discover/page.tsx
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { NGODiscoveryList } from "@/features/ngo-discovery/ngo-discovery-list";
+
+export const metadata = { title: "Discover Food | ShareBite BD" };
+
+export default function NGODiscoverPage() {
+  return <Suspense fallback={<div className="p-6"><Skeleton className="h-[40rem]" /></div>}><NGODiscoveryList /></Suspense>;
+}
