eb10b7904137a879be8d4a0ade9a20eadfce9ebb
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-09-23 17:04:54 +0600

    feat: add NGO notifications loading state

---
+++ src/app/(ngo)/ngo/notifications/loading.tsx
@@ -0,0 +1,5 @@
+import { RouteLoading } from "@/components/feedback/route-loading";
+
+export default function NGONotificationsLoading() {
+  return <RouteLoading label="Loading NGO notifications" />;
+}
